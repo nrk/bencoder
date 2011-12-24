8854959863e5053b8b5f0b1a473e93e73b05bfe3
Daniele Alessandri <suppakilla@gmail.com>
2011-12-24 16:51:03 +0100

    Add headers to source files.

--- autoload.php
+++ autoload.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Bencoder package.
+ *
+ * (c) Daniele Alessandri <suppakilla@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 spl_autoload_register(function($class) {
     $file = __DIR__.'/lib/'.strtr($class, '\\', '/').'.php';
     if (file_exists($file)) {
