de347518a43f35804388b2cdca0c485308863440
Daniele Alessandri <suppakilla@gmail.com>
2011-12-24 15:27:51 +0100

    Fix path for examples.

--- examples/decode_torrent.php
+++ examples/decode_torrent.php
@@ -4,7 +4,7 @@ require 'shared.php';
 
 use Bencoder\Bencode;
 
-$bencode = file_get_contents('xubuntu-10.10-alternate-amd64.iso.torrent');
+$bencode = file_get_contents(__DIR__.'/../examples/xubuntu-10.10-alternate-amd64.iso.torrent');
 $torrent = Bencode::unserialize($bencode);
 
 // Remove info.pieces as it would print too much binary garbage.
